92c8b9cade2524a690d28473be0fa4b7d27aae57
Nick Batsaras <batsarasnikos@gmail.com>
2017-05-23 20:04:54 +0300

    Classify projects and place them in the right directory

--- extractor.sh
+++ extractor.sh
@@ -1,6 +1,44 @@
 cd "$1"
-for file in *.tgz; do
+
+for file in *.tgz
+do
     exdir="${file%.tgz}"
     mkdir "$exdir"
     tar xvzf "$file" -C "$exdir" --strip-components=1
+done
+
+mkdir "C"
+mkdir "Java"
+mkdir "C++"
+
+for dir in ./*/
+do
+    dir=${dir%*/}
+    cd "$dir"
+
+    myarray=(`find ./ -maxdepth 1 -name "*.c"`)
+    if [ ${#myarray[@]} -gt 0 ]
+    then 
+        cd ".."
+        mv "$dir" ./C/
+        continue
+    fi
+
+    myarray=(`find ./ -maxdepth 1 -name "*.java"`)
+    if [ ${#myarray[@]} -gt 0 ]
+    then 
+        cd ".."
+        mv "$dir" ./Java/
+        continue
+    fi
+
+    myarray=(`find ./ -maxdepth 1 -name "*.cpp"`)
+    if [ ${#myarray[@]} -gt 0 ]
+    then 
+        cd ".."
+        mv "$dir" ./C++/
+        continue
+    fi
+
+    cd ".."
 done
