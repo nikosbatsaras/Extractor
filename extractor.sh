60990e03c3560bf2668c9c791c5fa42e468ac832
Nick Batsaras <batsarasnikos@gmail.com>
2017-08-09 09:49:52 +0300

    Remove max_depth

--- extractor.sh
+++ extractor.sh
@@ -20,11 +20,10 @@
 usage() {
     echo ""
     echo "Usage:"
-    echo "      ./extractor.sh [-h] [-d max_depth] -i <input-dir> -o <output-dir>"
+    echo "      ./extractor.sh [-h] -i <input-dir> -o <output-dir>"
     echo ""
     echo "Options:"
     echo "      -h   Show usage"
-    echo "      -d   Maximum depth to use with 'find' function (defaults to 10)"
     echo "      -i   Directory with compressed projects"
     echo "      -o   Directory to hold output"
     echo ""
@@ -33,13 +32,13 @@ usage() {
 }
 
 ##
-# Finds all source/header files until max_depth and copies the folder
-# containing them at the right depth, deleting any unwanted directories.
+# Finds all source/header files and copies the folder containing them at the
+# right depth, deleting any unwanted directories.
 #
 # @param $1 The programming language
 # @param $2 The output directory
 ##
-find_deep_sources() {
+restructure() {
     cd "$2/$1"
     local origin="$pwd"
 
@@ -51,22 +50,17 @@ find_deep_sources() {
 
     for project in "${projects[@]}"
     do
-        local i=1
         cd "$project"
-        while [ $i -le $max_depth ]
+
+        local directories=(`find "$PWD"/* -type d`)
+        for dir in "${directories[@]}"
         do
-            local directories=(`find "$PWD" -maxdepth $i -mindepth $i -type d`)
-            for dir in "${directories[@]}"
-            do
-                # Need to check if files exist inside
-                local count=`ls -1 "$dir"/*"$src" "$dir"/*"$hdr" \
-                    2>/dev/null | wc -l`
-                if [ $count != 0 ]
-                then 
-                    cp -r "$dir"/* "$project"
-                fi 
-            done
-            ((i++))
+            # Need to check if files exist inside
+            local num=`ls -1 "$dir"/*"$src" "$dir"/*"$hdr" 2>/dev/null | wc -l`
+            if [ $num != 0 ]
+            then 
+                cp -r "$dir"/* "$project"
+            fi 
         done
 
         # Delete unwanted directories
@@ -93,21 +87,21 @@ find_deep_sources() {
 # If directory contains .java files, it goes to a Java/ directory
 # ...
 #
-# Then, proceeds to call find_deep_sources to re-structure the classified
+# Then, proceeds to call restructure to re-structure the classified
 # directory.
 #
 # @param $1 The programming language
 # @param $2 The input directory
 # @param $3 The output directory
 ##
-classify_projects() {
+classify() {
     cd "$2"
     for dir in ./*/
     do
         local dir=${dir%*/}
         cd "$dir"
 
-        local files=(`find ./ -maxdepth $max_depth -name "${any_src[$1]}"`)
+        local files=(`find . -name "${any_src[$1]}"`)
         if [ ${#files[@]} -gt 0 ]
         then
             cd ".."
@@ -118,24 +112,16 @@ classify_projects() {
         cd ".."
     done
 
-    find_deep_sources "$1" "$3"
+    restructure "$1" "$3"
 }
 
 inputdir=""
 outputdir=""
 
 # Parse command-line arguments
-while getopts ":d:i:o:h" opt
+while getopts ":i:o:h" opt
 do
     case $opt in
-        d) 
-            if [ ! $OPTARG -gt 0 ]
-            then
-                echo "ERROR: Option -$opt must take a positive argument" >&2
-                exit 1
-            fi
-            max_depth=$OPTARG
-            ;; 
         i) 
             if [ ! -d "$OPTARG" ]
             then
@@ -178,13 +164,6 @@ fi
 
 if [ ! -z $check ]; then usage; fi
 
-##
-# Define maximum depth for use with 'find' function
-# Tune for more simple or complicated directory structures
-# If user did not specify a value for max_depth, give a default value of 10
-##
-if [ -z $max_depth ]; then max_depth=10; fi
-
 declare -A any_src=(["C"]="*.c" ["C++"]="*.cpp" ["Java"]="*.java")
 declare -A sources=(["C"]=".c"  ["C++"]=".cpp"  ["Java"]=".java" )
 declare -A headers=(["C"]=".h"  ["C++"]=".h"    ["Java"]=".java" )
@@ -204,6 +183,6 @@ mkdir "$outputdir/C++"
 mkdir "$outputdir/Java"
 
 # Classify extracted directories
-classify_projects "C"    "$inputdir" "$outputdir"
-classify_projects "C++"  "$inputdir" "$outputdir"
-classify_projects "Java" "$inputdir" "$outputdir"
+classify "C"    "$inputdir" "$outputdir"
+classify "C++"  "$inputdir" "$outputdir"
+classify "Java" "$inputdir" "$outputdir"
